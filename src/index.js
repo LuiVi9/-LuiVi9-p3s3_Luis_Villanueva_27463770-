d75db174aa2477af2e240b596d622b8687e070fd
Luis <luismanuelvillanueva76@gmail.com>
2024-06-08 23:29:28 -0400

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import indexRoutes from "./routes/index.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
